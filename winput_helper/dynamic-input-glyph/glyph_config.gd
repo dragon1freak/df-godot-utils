3721f7a4a467f81c7392083afc2e1122dcc98ac3
dragon1freak <dragon1freak2@gmail.com>
2025-01-06 09:56:17 -0500

    Dynamic glyphs

--- winput_helper/dynamic-input-glyph/glyph_config.gd
+++ winput_helper/dynamic-input-glyph/glyph_config.gd
@@ -5,9 +5,11 @@ class_name GlyphConfig
 @export_category("Glyphs")
 ## Texture used when Keyboard and Mouse is the active input
 @export var keyboard : Texture
-## Texture used when any controller is the active input, also the fallback if a controller specific glyph isnt provided
+## Texture used when any controller is the active input.  Is also the fallback if a controller specific glyph isnt provided
 @export var controller : Texture
 
+
+## Controller specific overrides, these are used in place of the above controller glyph if provided
 @export_group("Specific Controllers")
 @export var xbox : Texture
 @export var playstation : Texture
