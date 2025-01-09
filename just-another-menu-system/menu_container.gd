12d262f238d2e039376f982f7879c43f78413a14
dragon1freak <dragon1freak2@gmail.com>
2025-01-08 20:46:53 -0500

    Dropping multimenu for now

--- just-another-menu-system/menu_container.gd
+++ just-another-menu-system/menu_container.gd
@@ -3,14 +3,10 @@ class_name MenuContainer
 
 
 @export var start_open := false
-@export var mouse_mode : MouseFilter = MouseFilter.MOUSE_FILTER_PASS
+
 
 # Should add an is_open flag
 var is_open := false
-
-
-#func _ready() -> void:
-	#mouse_filter = mouse_mode
 
 
 func open() -> void:
--- just-another-menu-system/menu_router.gd
+++ just-another-menu-system/menu_router.gd
@@ -20,7 +20,9 @@ func register_children() -> void:
 	var menu_children = self.find_children("", "MenuContainer", true)
 	for child in menu_children:
 		MENUS[child.name.to_lower()] = child
-		if not child.start_open:
+		if child.start_open:
+			open_menu(child.name)
+		else:
 			child.close()
 
 
@@ -52,14 +54,15 @@ func go_back(open_next := true) -> void:
 	var next_menu = MENU_STACK.back()
 	if open_next and next_menu != null:
 		next_menu.open()
+		OPEN_MENUS.push_back(next_menu)
 
 
-func open_menu(menu_key : String, close_last_menu := true, on_open_callback : Callable = func(): pass) -> void:
+func open_menu(menu_key : String, close_open_menus := true, on_open_callback : Callable = func(): pass) -> void:
 	var target_menu = MENUS.get(menu_key.to_lower())
 	if target_menu == null or target_menu == get_last_menu():
 		return
 	
-	if close_last_menu:
+	if close_open_menus:
 		close_last_menu()
 	
 	target_menu.open()
@@ -84,8 +87,10 @@ func close_menu(menu_key : String, open_previous_menu := true, on_close_callback
 	if open_previous_menu and MENU_STACK.size() > 0:
 		var next_menu = MENU_STACK.back()
 		if next_menu != null:
-			next_menu.open()	
-	#target_menu.close()
+			next_menu.open()
+			OPEN_MENUS.push_back(next_menu)
+	
+	on_close_callback.call()
 
 
 func get_last_menu() -> MenuContainer:
