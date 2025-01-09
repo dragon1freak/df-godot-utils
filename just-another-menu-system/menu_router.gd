8140abfa627da09781347824960a6db598cd39d6
dragon1freak <dragon1freak2@gmail.com>
2025-01-08 21:21:42 -0500

    Version 1.0.0 of JAMS, opens one menu at a time, keeps a stack

--- just-another-menu-system/menu_router.gd
+++ just-another-menu-system/menu_router.gd
@@ -39,31 +39,14 @@ func clear_registered_menus() -> void:
 	MENUS = {}
 
 
-func go_back(open_next := true) -> void:
-	var current_menu = MENU_STACK.pop_back()
-	var current_open_menu = OPEN_MENUS.pop_back()
-	
-	if current_menu != null:
-		current_menu.close()
-	if current_open_menu != null:
-		current_open_menu.close()
-	
-	if MENU_STACK.size() <= 0:
-		return
-	
-	var next_menu = MENU_STACK.back()
-	if open_next and next_menu != null:
-		next_menu.open()
-		OPEN_MENUS.push_back(next_menu)
-
-
-func open_menu(menu_key : String, close_open_menus := true, on_open_callback : Callable = func(): pass) -> void:
+## Open the menu with the passed key function
+## [on_open_callback] is called after opening the target menu, if it exists
+func open_menu(menu_key : String, on_open_callback : Callable = func(): pass) -> void:
 	var target_menu = MENUS.get(menu_key.to_lower())
 	if target_menu == null or target_menu == get_last_menu():
 		return
 	
-	if close_open_menus:
-		close_last_menu()
+	close_last_menu()
 	
 	target_menu.open()
 	MENU_STACK.push_back(target_menu)
@@ -72,6 +55,8 @@ func open_menu(menu_key : String, close_open_menus := true, on_open_callback : C
 	on_open_callback.call()
 
 
+## Close the menu with the passed key function
+## [on_close_callback] is called after closing the target menu, if it exists
 func close_menu(menu_key : String, open_previous_menu := true, on_close_callback : Callable = func(): pass) -> void:
 	var target_menu = MENUS.get(menu_key.to_lower())
 	if target_menu == null or not target_menu.is_open:
@@ -82,7 +67,7 @@ func close_menu(menu_key : String, open_previous_menu := true, on_close_callback
 		return
 	
 	for i in range(OPEN_MENUS.size(), menu_index, -1):
-		go_back(false)
+		close_last_menu(true)
 	
 	if open_previous_menu and MENU_STACK.size() > 0:
 		var next_menu = MENU_STACK.back()
@@ -93,11 +78,42 @@ func close_menu(menu_key : String, open_previous_menu := true, on_close_callback
 	on_close_callback.call()
 
 
+## Close all other menus, clear menu history, open the menu with the passed key
+func go_to(menu_key : String) -> void:
+	close_all_menus()
+	open_menu(menu_key)
+
+
+## Go back one step in the menu history, closing the current menu
+func go_back(open_next := true) -> void:
+	close_last_menu(true)
+	
+	if MENU_STACK.size() <= 0:
+		return
+	
+	var next_menu = MENU_STACK.back()
+	if open_next and next_menu != null:
+		next_menu.open()
+		OPEN_MENUS.push_back(next_menu)
+
+
+## Get the most recent menu in the stack
 func get_last_menu() -> MenuContainer:
 	return MENU_STACK.back() if MENU_STACK.size() > 0 else null
 
 
-func close_last_menu() -> void:
+## Close most recent menu
+## [remove_from_stack] if true, also removes the most recent menu from the stack
+func close_last_menu(remove_from_stack := false) -> void:
 	var last_menu = OPEN_MENUS.pop_back()
 	if last_menu != null:
 		last_menu.close()
+	if remove_from_stack:
+		MENU_STACK.pop_back()
+
+
+func close_all_menus() -> void:
+	for menu in OPEN_MENUS:
+		menu.close()
+	OPEN_MENUS = []
+	MENU_STACK = []
